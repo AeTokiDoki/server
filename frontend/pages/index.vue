31a0602279c1562be3f58c247e4350827a5490a2
Sergey <podshiblov2015@yandex.ru>
2022-01-26 16:12:59 +0300

    hello world!

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,9 +1,26 @@
 <template>
-  <Tutorial/>
+  <div>
+    {{ hello }}
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: 'IndexPage'
-}
+  data() {
+    return {
+      hello: null,
+    };
+  },
+  async mounted() {
+    try {
+      const response = await axios.get("http://localhost:1337/main");
+      this.hello = response.data.hello;
+    } catch (error) {
+      this.erorr = error;
+      console.log(this.erorr);
+    }
+  },
+};
 </script>
